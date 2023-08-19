c2e1f9830adb186e071f880f733d1230512b48f3
Felipe Cruz <felipeiagomcruz@hotmail.com>
2023-08-19 22:32:47 +0000

    :hammer: refactor: Refatoração do código, consulta por estado, proteção SQL Injection

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -7,26 +7,48 @@ import java.sql.Statement;
 
 
 public class AppBd {
+    private static final String PASSWORD = "";
+    private static final String USERNAME = "gitpod";
+    private static final String JDBC_URL = "jdbc:postgresql://localhost/postgres";
+
     public static void main(String[] args) {
+      new AppBd();
+      
+    }
+
+    public AppBd(){
+        carregarDriverJDBC(); 
+        try (Connection conn = getConnection()) {
+        // listarEstados(conn);
+        localizarEstado(conn,"TO");
+        }
+        catch (SQLException e) {
+            System.err.println("Não foi possível conectar ao banco de dados:"+ e.getMessage());        }
+    }
 
 
-        try {
-            Class.forName("org.postgresql.Driver");
+    private void localizarEstado(Connection conn, String uf) {
+        try{
+              // String query = "SELECT * from estado where uf ='"+uf+"'"; //suscetível a SQL Injection
+            String SQLquery = "SELECT * from estado where uf = ?";
+            var statement = conn.prepareStatement(SQLquery);
+            statement.setString(1, uf);
+            var result = statement.executeQuery();
+            if(result.next()){
+                System.out.printf("Id: %d | Nome: %s | UF: %s \n",result.getInt("id"),result.getString("nome"),result.getString("uf"));
+            }
+        }   
+        catch(SQLException e){
+            System.err.printf("Não foi possível localizar o estado %s |" + e.getMessage(),uf);
+        }
+    }
 
-        } 
-        catch (ClassNotFoundException e) {
-            // TODO: handle exception
-            System.err.println("Não foi possível carregar a bibblioteca para acessa ao banco de dados." + e.getMessage());
-        } 
-
-
-        Statement statement = null;
-
-        try (Connection conn = DriverManager.getConnection("jdbc:postgre://localhost/postgres", "gitpod", "");) {
+    private void listarEstados(Connection conn) {
+        try{
                 
             System.out.println("Conexão com o banco realizada com sucesso.");
     
-            statement = conn.createStatement();
+            var statement = conn.createStatement();
     
             var result = statement.executeQuery("SELECT * from estado");
     
@@ -35,12 +57,23 @@ public class AppBd {
             } 
         } 
         catch (SQLException e) {
-            if(statement == null)
                 System.err.println("Não foi possível conectar ao banco de dados:"+ e.getMessage());
-            else   
-                System.out.println("Não foi possível realizar a consulta ao banco de dados." + e.getMessage());
         }
-        
+    }
+
+    private Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+    }
+
+    private void carregarDriverJDBC() {
+        try {
+            Class.forName("org.postgresql.Driver");
+
+        } 
+        catch (ClassNotFoundException e) {
+            // TODO: handle exception
+            System.err.println("Não foi possível carregar a bibblioteca para acessa ao banco de dados." + e.getMessage());
+        }
     }
     
 }
